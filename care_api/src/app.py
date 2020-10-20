f84d2d90aff64429ead2154b574fdc7a140481f9
Austin Keppers <akeppers21@gmail.com>
2020-10-20 16:15:06 -0400

    Added skeleton routing code

---
+++ care_api/src/app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+
+# Instantiate Flask app
+care_app = Flask('care_api')
+
+@care_app.route('/account/<path:action>', methods=['POST', 'DELETE'])
+def update_account(action):
+    pass
+
+@care_app.route('/group/<int:group_id>', methods=['GET', 'PUT', 'POST', 'DELETE'])
+def change_groups(group_id):
+    pass
+
+@care_app.route('/group/recommend/<int:recommend_id>', methods=['POST'])
+def add_recommendation(recommend_id):
+    pass
+
+@care_app.route('/group/recommend-approve/<int:recommend_id>', methods=['POST'])
+def approve_recommendation(recommend_id):
+    pass
+
+@care_app.route('/report/<int:report_id>', methods=['GET', 'PUT', 'POST', 'DELETE']) 
+def update_reports(report_id):
+    pass
+
+@care_app.route('/report/<path:view_criteria>', methods=['GET']) 
+def view_reports_by_criteria(view_criteria):
+    pass
+
+@care_app.route('/report/spam/<int:report_id>', methods=['POST'])
+def is_report_spam(report_id):
+    pass
+
+@care_app.route('/faq/<int:question_id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def update_faq(question_id):
+    pass
+
+@care_app.route('/faq/request/<int:request_id>', methods=['GET', 'POST', 'DELETE'])
+def manmage_faq_request(request_id):
+    pass
