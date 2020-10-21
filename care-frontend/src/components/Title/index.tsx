38245cbcd619f3a7c8b59880f946d583210ba395
Matthew Yoon <43835154+mly18@users.noreply.github.com>
2020-10-21 13:10:00 -0400

    Style changes to FAQ page

--- care-frontend/src/components/Title/index.tsx
+++ care-frontend/src/components/Title/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+});
 
 export const Title = () => {
-  return (
-    <div style={{ width: '100%' }}>
-      <Box component="span" visibility="visible" p={1} m={1} bgcolor="background.paper">
-        Frequently Asked Questions
-      </Box>
-    </div>
-  );
+    const classes = useStyles();
+
+    return (
+      <div className={classes.root}>
+        <Typography variant="h5" gutterBottom>
+            Frequently Asked Questions
+        </Typography>
+      </div>
+    );
 }
