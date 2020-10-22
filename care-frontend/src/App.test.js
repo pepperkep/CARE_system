4ff33449a51490518f4be91c537fcca4fd386bee
Matthew Yoon <43835154+mly18@users.noreply.github.com>
2020-10-22 13:08:00 -0400

    Installed enzyme-adapter

--- care-frontend/src/App.test.js
+++ care-frontend/src/App.test.js
@@ -1,15 +1,8 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
-import Account from "./Account";
-import App from "./App";
-import toJson from "enzyme-to-json";
+import Title from "./Title";
 
-it("renders without crashing", () => {
-    shallow(<Title />);
-  });
-  
-  it("renders Account header", () => {
-    const wrapper = shallow(<Title />);
-    const welcome = <h5>Frequently Asked Questions</h5>;
-    expect(wrapper.contains(welcome)).toEqual(true);
+it("renders correctly", () => {
+    const wrapper = mount(<Title />);
+    expect(wrapper.state("error")).toEqual(null);
   });
