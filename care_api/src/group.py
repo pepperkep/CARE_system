8b8f72b49abfc6acef12bdf94b10553405e539a9
Mamadi Jallow <mxj316@case.edu>
2020-10-22 12:50:04 -0400

    Fixed method header mistakes in group.py

--- care_api/src/group.py
+++ care_api/src/group.py
@@ -5,7 +5,7 @@ import datetime as datetime
 from database import database_handler as db
 from app import care_app
 
-        
+
 
 @care_app.route('/group/<int:group_id>', methods=[ 'POST'])
 def add_group(group_id, name, description):
@@ -20,5 +20,5 @@ def update_group(report_id):
     pass
 
 care_app.route('/report/<int:report_id>', methods=['GET'])
-def view_report(report_id):
+def view_group(group_id):
     pass
