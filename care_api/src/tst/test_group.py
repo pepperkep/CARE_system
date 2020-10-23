dede710e015282a659052d9cdb8ce831c66b4490
Mamadi Jallow <mxj316@case.edu>
2020-10-23 10:26:08 -0400

    Added in tests for the group and report classes

---
+++ care_api/src/tst/test_group.py
@@ -0,0 +1,46 @@
+import unittest
+from src.group import Group
+from src.database.database_handler import DatabaseHandler
+from unittest.mock import MagicMock, patch
+from flask import json, Flask
+
+
+app = Flask(__name__)
+
+class TestGroup(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_db_handler = MagicMock()
+        self.group = User(self.mock_db_handler)
+
+    def test_add_group(self):
+        self.mock_db_handler.create.return_value = 'test'
+        expected_result = {'name': 'marx', 'description':'group','_id':  45}
+        with app.test_request_context(45, json=json.dumps({'name': 'marx', 'description': 'group'})):
+            actual_result = self.group.add_group(45)
+            self.assertEqual(expected_result, actual_result)
+
+    def test_view_group:
+        expected_result = {'name': 'marx', 'description':'group','_id':  45}
+        with app.test_request_context(45, json=json.dumps({'name': 'marx', 'description': 'group'})):
+            actual_result = self.mock_db_handler.find(45)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_delete_group(self):
+        self.mock_db_handler.find.return_value = 'foo'
+        expected_result = {'_id':  45}
+        with app.test_request_context(45):
+            actual_result = self.group.delete_group('45')
+            self.assertEqual(expected_result, actual_result)
+
+    def test_update_group(self):
+        self.mock_db_handler.create.return_value = 'test'
+        expected_result = {'name': 'marx', 'description':'group','_id':  45}
+        with app.test_request_context(45, json=json.dumps({'name': 'marx', 'description': 'group'})):
+            actual_result = self.group.update_group(45)
+            self.assertEqual(expected_result, actual_result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ care_api/src/tst/test_report.py
@@ -0,0 +1,44 @@
+import unittest
+from src.report import Report
+from src.database.database_handler import DatabaseHandler
+from unittest.mock import MagicMock, patch
+from flask import json, Flask
+
+
+app = Flask(__name__)
+
+class TestReport(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_db_handler = MagicMock()
+        self.group = User(self.mock_db_handler)
+
+    def test_add_report(self):
+        self.mock_db_handler.create.return_value = 'test'
+        expected_result = {'content': 'COVID', 'timestamp':'07/04/20','_id':  45}
+        with app.test_request_context(45, json=json.dumps({'content': 'COVID', 'timestamp': '07/04/20'})):
+            actual_result = self.report.add_report(45)
+            self.assertEqual(expected_result, actual_result)
+
+    def test_view_report:
+        expected_result = {'content': 'COVID', 'timestamp':'07/04/20','_id':  45}
+        with app.test_request_context(45, json=json.dumps({'content': 'COVID', 'timestamp':'07/04/20'})):
+            actual_result = self.mock_db_handler.find(45)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_delete_report(self):
+        self.mock_db_handler.find.return_value = 'foo'
+        expected_result = {'_id':  45}
+        with app.test_request_context(45):
+            actual_result = self.report.delete_report('45')
+            self.assertEqual(expected_result, actual_result)
+
+    def test_update_report(self):
+        self.mock_db_handler.create.return_value = 'test'
+        expected_result = {'content': 'COVID', 'timestamp':'07/04/20','_id':  45}
+        with app.test_request_context(45, json=json.dumps({'content': 'COVID', 'timestamp':'07/04/20'})):
+            actual_result = self.report.update_report(45)
+            self.assertEqual(expected_result, actual_result)
+
+if __name__ == '__main__':
+    unittest.main()
