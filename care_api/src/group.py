0ae6b4decdcc487bf56eef0d60df23ec91eb61ca
Mamadi Jallow <mxj316@case.edu>
2020-10-23 10:30:43 -0400

    Minor changes to report and group class

--- care_api/src/group.py
+++ care_api/src/group.py
@@ -11,13 +11,13 @@ class Group:
         self.db_handler = db_handler
 
 
-    def add_group(group_id, name, description):
+    def add_group(group_id):
         group_doc =  {"_id":group_id,
             "name":request.form['name'],
             "description":request.form['description']
             }
         db.create(group_doc)
-
+        return group_doc
 
     def delete_group(group_id):
         db.delete(db.find({"_id":group_id}))
@@ -28,7 +28,7 @@ class Group:
         new_descritpion = { "$set": {'description': request.form['new_description']}}
         db.update(db.find({"_id":group_id}), new_name)
         db.update(db.find({"_id":group_id}), new_description)
-
+        return db.find({"_id":group_id})
 
     def view_group(group_id):
-        print(db.find({"_id":group_id}))
+        return db.find({"_id":group_id})
