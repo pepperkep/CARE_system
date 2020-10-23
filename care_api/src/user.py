d495b392868c3fe68e168f2b424618f6c32dcbf9
Austin Keppers <akeppers21@gmail.com>
2020-10-23 03:52:35 -0400

    Added login and logout methods

--- care_api/src/user.py
+++ care_api/src/user.py
@@ -1,4 +1,4 @@
-from flask import request, abort, json
+from flask import request, abort, json, session
 from database.database_handler import DatabaseHandler
 
 class User:
@@ -14,6 +14,8 @@ class User:
         if action == 'signup':
             return self.signup(*params)
         if action == 'login':
+            return self.login(*params)
+        if action == 'logout':
             return self.login(*params)
         if action == 'change_password':
             return self.change_password(*params)
@@ -36,11 +38,24 @@ class User:
         return response_dict
 
     def login(self, login_id):
-        return login_id
+        try:
+            query = {'_id': login_id}
+        except KeyError:
+            abort(400)
+        result = self.db_handler.find_one('user', query, change)
+        if request.form['username'] == result['username'] and request.form['password'] == result['password']:
+            session[login_id] = login_id
+        else:
+            abort(401)
+        return {}
+
+    def logout(self, login_id):
+        session.pop(login_id, None)
+        return {}
 
     def change_password(self, username):
         try:
-            query = {'_id': request.form['id'],
+            query = {'_id': request.form['id']}
         except KeyError:
             abort(400)
         change = { "$set": {'password': request.form['new_password']}}
@@ -48,18 +63,11 @@ class User:
         return query
         
     def access_user(self, account_id):
-        try:
-            query = {'_id': account_id}
-        except KeyError:
-            abort(400)
-        self.db_handler.find_one('user', query, change)
-        return query
+        query = {'_id': account_id}
+        result =  self.db_handler.find_one('user', query, change)
+        return result
 
     def delete_account(self, account_id):
-        try:
-            query = {'_id': account_id}
-        except KeyError:
-            abort(400)
         query = {'_id': account_id}
         self.db_handler.delete('user', query, change)
         return query
