e0d39824cf637eded40f809ab27801bf37c6f167
terminatorlkf <likefan2009@sina.com>
2020-11-03 20:58:06 -0500

    Fix testing for navigation bar

--- care-frontend/src/components/NavigationBar/__tests__/NavigationBar.test.tsx
+++ care-frontend/src/components/NavigationBar/__tests__/NavigationBar.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { NavigationBar } from '..';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
-        <NavigationBar />,
+        <Router>
+            <Switch>
+                <NavigationBar />
+            </Switch>
+        </Router>,
         div);
 });
