2934c031a39fb3b308789fec14e8ba431fe2a930
Mamadi Jallow <mxj316@case.edu>
2020-11-13 12:03:11 -0500

    Approve recommendation method written

--- care_api/src/app.py
+++ care_api/src/app.py
@@ -40,7 +40,8 @@ def add_recommendation(recommend_id):
 
 @care_app.route('/group/recommend-approve/<int:recommend_id>', methods=['POST'])
 def approve_recommendation(recommend_id):
-    pass
+    group = Group(DatabaseHandler(config_path, config['mongodb']['db_name']))
+    return group.add_group(recommend_id)
 
 @care_app.route('/report/<int:report_id>', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def update_reports(report_id):
