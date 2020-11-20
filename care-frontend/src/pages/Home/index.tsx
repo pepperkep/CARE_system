920b946accd40eab39b420cbeaccc3a1e9ebd539
Matthew Yoon <43835154+mly18@users.noreply.github.com>
2020-11-19 20:12:41 -0500

    Fixed issue with Home page reports

--- care-frontend/src/pages/Home/index.tsx
+++ care-frontend/src/pages/Home/index.tsx
@@ -1,30 +1,27 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { ReportContext } from '../../context/ReportContext';import { Button } from '@material-ui/core';
+import { Report } from '../../interfaces/Report';
 import { ReportCardList } from '../../components';
-import { GroupContext } from '../../context/GroupContext';
-import { ResultCardList } from '../../components';
 import axios from 'axios';
 
 export const Home = () => {
-    const { setReportList, reportList } = useContext(ReportContext);
-
-    const [] = useState<boolean>(true);
+    const [reports, setReports] = useState<Report[]>([]);
 
     useEffect(() => {
-        const getReportList = async () => {
+        const getReports = async () => {
             const response = await axios.get('http://127.0.0.1:5000/report/recents/3');
 
             if (response.status == 200) {
-                setReportList(response.data);
+                setReports(response.data);
             }
         }
 
-        getReportList();
-    }, [setReportList]);
+        getReports();
+    }, [setReports]);
 
     return (
         <div>
-            <ReportCardList reportList={reportList} />
+            <ReportCardList reportList={reports} />
         </div>
     )
 }
